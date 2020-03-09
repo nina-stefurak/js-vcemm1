c8f4142f6460e6149adbd8096323a481beefa95e
nina-stefurak <nina.stefurak06@gmail.com>
2020-03-09 19:59:39 +0100

    Array sum, korzinka i jabloczki

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Import stylesheets
 import './style.css';
-
+var array = [7, 3, 4]
+function simpleArraySum(korzinka) {
+    var sum = 0;
+    korzinka.forEach(jabloczko => sum += jabloczko)
+    return sum
+}
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter Hello</h1>`;
+appDiv.innerHTML = `<h1>JS Starter Hello</h1>` + simpleArraySum(array);
